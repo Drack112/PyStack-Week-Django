1281a521eadafcd7ddffe9cf48de338175d4cace
Drack <61944386+Drack112@users.noreply.github.com>
2022-01-14 11:55:35 -0300

    Update Final

--- Agendamento Imobiliario/plataforma/urls.py
+++ Agendamento Imobiliario/plataforma/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from .views import home, imovel
+from .views import home, imovel, agendar_visitas, agendamentos, cancelar_agendamento
 
 urlpatterns = [
     path("", home, name="home"),
     path("imovel/<str:id>", imovel, name="imovel"),
+    path("agendar_visitar", agendar_visitas, name="agendar_visitas"),
+    path("agendamentos", agendamentos, name="agendamentos"),
+    path(
+        "cancelar_agendamento/<str:id>",
+        cancelar_agendamento,
+        name="cancelar_agendamento",
+    ),
 ]
--- Agendamento Imobiliario/plataforma/views.py
+++ Agendamento Imobiliario/plataforma/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 from .models import *
 
@@ -45,3 +46,28 @@ def imovel(request, id):
     return render(
         request, "imovel.html", {"imovel": imovel, "sugestoes": sugestoes, "id": id}
     )
+
+
+def agendar_visitas(request):
+    usuario = request.user
+    dia = request.POST.get("dia")
+    horario = request.POST.get("horario")
+    id_imovel = request.POST.get("id_imovel")
+
+    visitas = Visitas(imovel_id=id_imovel, usuario=usuario, dia=dia, horario=horario)
+
+    visitas.save()
+
+    return redirect("/agendamentos")
+
+
+def agendamentos(request):
+    visitas = Visitas.objects.filter(usuario=request.user)
+    return render(request, "agendamentos.html", {"visitas": visitas})
+
+
+def cancelar_agendamento(request, id):
+    visitas = get_object_or_404(Visitas, id=id)
+    visitas.status = "C"
+    visitas.save()
+    return redirect("/agendamentos")
