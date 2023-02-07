5b5d37b9727c008a86ac32c8ccda925df25eac3f
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-06 21:39:37 -0300

    :sparkles: model of pacients and data on sql data

--- Nutri-Lab/plataforma/admin.py
+++ Nutri-Lab/plataforma/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pacientes, DadosPaciente, Refeicao, Opcao
+
+admin.site.register(Pacientes)
+admin.site.register(DadosPaciente)
+
+admin.site.register(Refeicao)
+admin.site.register(Opcao)
--- Nutri-Lab/plataforma/models.py
+++ Nutri-Lab/plataforma/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Pacientes(models.Model):
+    choices_sexo = (("F", "Feminino"), ("M", "Maculino"))
+    nome = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=1, choices=choices_sexo)
+    idade = models.IntegerField()
+    email = models.EmailField()
+    telefone = models.CharField(max_length=19)
+    nutri = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+
+class DadosPaciente(models.Model):
+    paciente = models.ForeignKey(Pacientes, on_delete=models.CASCADE)
+    data = models.DateTimeField()
+    peso = models.IntegerField()
+    altura = models.IntegerField()
+    percentual_gordura = models.IntegerField()
+    percentual_musculo = models.IntegerField()
+    colesterol_hdl = models.IntegerField()
+    colesterol_ldl = models.IntegerField()
+    colesterol_total = models.IntegerField()
+    trigliceridios = models.IntegerField()
+
+    def __str__(self):
+        return f"Paciente({self.paciente.nome}, {self.peso})"
+
+
+class Refeicao(models.Model):
+    paciente = models.ForeignKey(Pacientes, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=50)
+    horario = models.TimeField()
+    carboidratos = models.IntegerField()
+    proteinas = models.IntegerField()
+    gorduras = models.IntegerField()
+
+    def __str__(self):
+        return self.titulo
+
+
+class Opcao(models.Model):
+    refeicao = models.ForeignKey(Refeicao, on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to="opcao")
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.descricao
