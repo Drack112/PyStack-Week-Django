a2c74a0293f208654c1f77e084cbb28a2e1a363d
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-06 21:41:39 -0300

    :sparkles: routes and views of plataforma app

--- Nutri-Lab/plataforma/urls.py
+++ Nutri-Lab/plataforma/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
 
-from .views import pacientes
+from . import views
 
-urlpatterns = [path("pacientes", pacientes, name="pacientes")]
+urlpatterns = [
+    path("pacientes", views.pacientes, name="pacientes"),
+    path("dados_paciente", views.dados_paciente_listar, name="dados_paciente_listar"),
+    path("dados_paciente/<str:id>", views.dados_paciente, name="dados_paciente"),
+    path("grafico_peso/<str:id>", views.grafico_peso, name="grafico_peso"),
+    path(
+        "plano_alimentar_listar",
+        views.plano_alimentar_listar,
+        name="plano_alimentar_listar",
+    ),
+    path("plano_alimentar/<str:id>", views.plano_alimentar, name="plano_alimentar"),
+    path("refeicao/<str:id_paciente>", views.refeicao, name="refeicao"),
+    path("opcao/<str:id_paciente>", views.opcao, name="opcao"),
+]
