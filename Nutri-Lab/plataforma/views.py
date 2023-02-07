ced3bd9529ec02aaf78220c9d9d313e6caa0e21b
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-06 21:04:22 -0300

    :sparkles: improve the if_user.is_authenticated statement to work with authentication

--- Nutri-Lab/plataforma/views.py
+++ Nutri-Lab/plataforma/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+
+
+@login_required(login_url="/auth/logar")
 def pacientes(request):
     return HttpResponse(request, "Hello World!")
