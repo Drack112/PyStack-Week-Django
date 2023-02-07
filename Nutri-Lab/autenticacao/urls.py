48f1a63a6f250ad762d8b70b2e8988eb6dbde10e
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-06 21:33:59 -0300

    :bug: slash on the urls file

--- Nutri-Lab/autenticacao/urls.py
+++ Nutri-Lab/autenticacao/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("cadastro/", views.cadastro, name="cadastro"),
-    path("logar/", views.logar, name="logar"),
-    path("sair/", views.sair, name="sair"),
-    path("ativar_conta/<str:token>/", views.ativar_conta, name="ativar_conta"),
+    path("cadastro", views.cadastro, name="cadastro"),
+    path("logar", views.logar, name="logar"),
+    path("sair", views.sair, name="sair"),
+    path("ativar_conta/<str:token>", views.ativar_conta, name="ativar_conta"),
 ]
